7a4c6c192147509238b6765800a7771744e6c299
Ashok Kaushik <kaushikashok45@gmail.com>
2021-12-28 16:15:37 +0530

    Fixed scrollbar bugs.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import Footer from "./components/Footer";
 import Header from "./components/header";
 import MainContent from "./components/MainContent";
-import { Scrollbars } from 'react-custom-scrollbars';
+import "./scrollbarContainer.css";
 
 function App() {
   return (
-    <Scrollbars style={{ width: "100vw", height: "100vh" }}  renderTrackVertical={({style, ...props}) =>
-    <div {...props} className={style.scrollTrackVertical} style={{...style, backgroundColor: 'white', right: '2px', bottom: '2px', top: '2px', borderRadius: '3px', width: '5px'}}/>
-}>
+   
      <div className="App" >
+       <div className="body">
       
           {/* <h1>Hiiiiiiiiii</h1> */}
           <Header/>
           <MainContent/>
           <Footer/>
-      
+      </div>
      </div>
-    </Scrollbars>
+   
   );
 }
 
