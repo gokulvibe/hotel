ff296e75c3b056640faff182db8b9873d1da32c2
Gokul Raam <gokulraamofficial@gmail.com>
2022-01-18 17:10:28 +0530

    Decomposed Header to sub components

---
+++ src/Compnents2/Header/ContactNumber.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function ContactNumber() {
+    return (
+            <div
+                                            className="elementor-column elementor-col-25 elementor-top-column elementor-element elementor-element-fcec661"
+                                            data-id="fcec661"
+                                            data-element_type="column"
+                                            data-settings='{"craftcoffee_ext_is_scrollme":"false","craftcoffee_ext_is_smoove":"false","craftcoffee_ext_is_parallax_mouse":"false","craftcoffee_ext_is_infinite":"false","craftcoffee_ext_is_fadeout_animation":"false"}'
+                                        >
+                                            <div className="elementor-column-wrap elementor-element-populated">
+                                                <div className="elementor-widget-wrap">
+                                                    <div
+                                                        className="elementor-element elementor-element-ff52274 elementor-align-center elementor-widget-tablet__width-auto elementor-hidden-phone elementor-widget elementor-widget-button"
+                                                        data-id="ff52274"
+                                                        data-element_type="widget"
+                                                        data-settings='{"craftcoffee_ext_is_scrollme":"false","craftcoffee_ext_is_smoove":"false","craftcoffee_ext_is_parallax_mouse":"false","craftcoffee_ext_is_infinite":"false","craftcoffee_ext_is_fadeout_animation":"false"}'
+                                                        data-widget_type="button.default"
+                                                    >
+                                                        <div className="elementor-widget-container">
+                                                            <div className="elementor-button-wrapper">
+                                                                <a href="tel:234.567.3455" className="elementor-button-link elementor-button elementor-size-sm" role="button">
+                                                                    <span className="elementor-button-content-wrapper">
+                                                                        <span className="elementor-button-text">234.567.3455</span>
+                                                                    </span>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div
+                                                        className="elementor-element elementor-element-0b79fea elementor_mobile_nav elementor-widget__width-auto elementor-hidden-desktop elementor-view-default elementor-widget elementor-widget-icon"
+                                                        data-id="0b79fea"
+                                                        data-element_type="widget"
+                                                        data-settings='{"craftcoffee_ext_is_scrollme":"false","craftcoffee_ext_is_smoove":"false","craftcoffee_ext_is_parallax_mouse":"false","craftcoffee_ext_is_infinite":"false","craftcoffee_ext_is_fadeout_animation":"false"}'
+                                                        data-widget_type="icon.default"
+                                                    >
+                                                        <div className="elementor-widget-container">
+                                                            <div className="elementor-icon-wrapper">
+                                                                <a className="elementor-icon" href=""> <i aria-hidden="true" className="fas fa-ellipsis-v"></i> </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+    )
+}
+
+export default ContactNumber;
