9e93207f5d33f6a1d4d260ab1ae06274bbb0373b
kaushikashok45 <kaushikashok45@gmail.com>
2022-01-28 16:21:31 +0530

    Tweaked color scheme of tab switcher ui on signuplogin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Cart from 'Pages/Cart';
+import Cart from "Pages/Cart";
 import Home from "Pages/Home";
-import Menu from 'Pages/Menu';
-import LoginSignup from './Pages/LoginSignup';
+import Menu from "Pages/Menu";
+import LoginSignup from "Pages/LoginSignup";
 import "scrollbarContainer.css";
 
 
