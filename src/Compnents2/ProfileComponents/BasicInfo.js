bd127aa5fad4521cde34e692787f7dded50dd2b6
kaushikashok45 <kaushikashok45@gmail.com>
2022-01-29 23:03:52 +0530

    Tweaked Responsiveness of loginsignup com[ponent

--- src/Compnents2/ProfileComponents/BasicInfo.js
+++ src/Compnents2/ProfileComponents/BasicInfo.js
@@ -5,28 +5,34 @@ import "scrollbarContainer.css"
 
 function BasicInfo(){
     return(
-        <div className='BasicInfo' >
+      <div className='BasicInfo' >
             <div id="profileCard">
-              <img src={Avatar} alt="Avatar"/>
-              <div id="boxed">
-                <br/>  
-                <h1><mark style={{fontFamily:"Arkipelago",textTransform:"none"}}>Ashok Kaushik</mark></h1>
+              <div className='column1'>
+                <img src={Avatar} alt="Avatar"/>
+                <div id="boxed">
+                  <br/>  
+                  <h1><mark style={{fontFamily:"Arkipelago",textTransform:"none"}}>Ashok Kaushik</mark></h1>
+                </div>
+                <br/>
+                <h3>&#128231;-kaushikashok45@gmail.com</h3>
+                <h3>&#128241;-7358427164</h3>
               </div>
-              <br/>
-              <h3>&#128231;-kaushikashok45@gmail.com</h3>
-              <h3>&#128241;-7358427164</h3>
-              <br/>
-              <h1><mark style={{fontFamily:"Arkipelago",textTransform:"none"}}>Explore</mark></h1>
-              <div class="table">
-               <ul>
-                  <li><a href="http://allisondskinner.com">Your Orders</a></li>
-                  <li><a href="/menu">Our Menu</a></li>
-                  <li><a href="/cart">Your Cart</a></li>
-                  <li><a href="http://allisondskinner.com">Give Feedback</a></li>
-               </ul>
-              </div> 
+              <div className='column2'>
+                <h1 style={{textAlign:"center"}}><mark style={{fontFamily:"Arkipelago",textTransform:"none"}}>Explore</mark></h1>
+                <div className="table">
+                 <ul className='options'>
+                    <li className='oli'><a href="http://allisondskinner.com">Your Orders</a></li>
+                   
+                    <li className='oli'><a href="/menu">Give Feedback</a></li>
+                    
+                    <li className='oli'><a href="/cart">Your Cart</a></li>
+                    
+                    <li className='oli'><a href="http://allisondskinner.com">Our Menu</a></li>
+                 </ul>
+                </div>
+              </div>   
             </div>
-        </div>
+      </div>
     )
 }
 
