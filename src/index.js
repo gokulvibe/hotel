66f6ff7ce663ec0e1ea1d2cdee902da6526331fa
Gokul Raam <gokulraamofficial@gmail.com>
2022-03-28 00:39:37 +0530

    Made the app as pwa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 
 ReactDOM.render(
@@ -15,4 +17,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
+// serviceWorker.register();
+
+serviceWorkerRegistration.register();
